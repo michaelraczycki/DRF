272aa1451c1c2aa2af17beadfa88601e5952fd91
michaelraczycki <michalr265@gmail.com>
2023-10-06 08:58:09 +0200

    finishing CRUD

--- backend/products/views.py
+++ backend/products/views.py
@@ -24,6 +24,26 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     serializer_class = ProductSerializer
     lookup_field = "pk"
 
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "pk"
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "pk"
+
+    def perform_destroy(self, instance):
+        #instance
+        super().perform_destroy(instance)
+
+
 
 @api_view(["GET", "POST"])
 def product_alt_view(request, pk=None, *args, **kwargs):
